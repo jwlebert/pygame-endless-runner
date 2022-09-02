eda5400cc7f977bac4a898206cd00bad9871dd24
JWL <j.lebert4402@gmail.com>
2022-09-02 15:26:54 -0400

    changed main.py to reflect the changes made to settings

--- main.py
+++ main.py
@@ -1,17 +1,7 @@
 from src.Game import Game
 
-# Settings dictionary to centralize all the game parameters, such as the size
-# of the screen and it's title.
-settings = {
-	"game_title": "Endless Runner", # ! TEMPLATE: decide on a name for the end product
-	"FPS": 60,
-	"screen_size": (900, 600),
-	# "screen_size": (1920, 1080),
-	"scroll_factor": 0.3, # % of screen per second
-}
-
 def main():
-	game = Game(settings)
+	game = Game()
 	game.start()
 
 if __name__ == "__main__":
